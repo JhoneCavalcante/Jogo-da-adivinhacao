7815280fac27f14aa7f463fc0c5eb712e3f3dfdd
Jhone Cavalcante <138171946+JhoneCavalcante@users.noreply.github.com>
2024-06-24 17:11:59 -0300

    Registrando as tentativas
    utilizando uma variável para registrar o número de tentativas do jogador, até acertar o número secreto.

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 alert("Boas-vindas ao jogo do número secreto");
 let numeroSecreto = Math.floor(Math.random() * 10) +1;
-console.log('numeroSecreto')
-let chute
+console.log('numeroSecreto');
+let chute;
+let tentativas = 1;
 while (chute != numeroSecreto) {
    chute = prompt('Escolha um número entre 1 e 10'); 
     if (chute == numeroSecreto) {
-        alert(`você descobriu o número secreto ${numeroSecreto}! Parabéns!`);
+        alert(`você descobriu o número secreto ${numeroSecreto}com ${tentativas} tentativas.`);
     } else {
         if (chute > numeroSecreto) {
             alert(`O número secreto é menor do que ${chute}`);
         } else {
             alert (`O número secreto é maior do que ${chute}`);
         }
+        tentativas++;
     }
 }
