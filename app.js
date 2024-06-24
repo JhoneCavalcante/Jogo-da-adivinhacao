177a350dabe55f0d8a49ffcd76fbbfe41c2d4422
Jhone Cavalcante <138171946+JhoneCavalcante@users.noreply.github.com>
2024-06-24 16:43:12 -0300

    Criando as estruturas em HTML, estilo e a lógica
    Foi criado em HTML, toda a estrutura;
    Em CSS o visual foi personalizado;
    Em JS foi feita a lógica de programação.

---
+++ app.js
@@ -0,0 +1,17 @@
+alert("Boas-vindas ao jogo do número secreto");
+
+let numeroSecreto = 8
+console.log('numeroSecreto')
+
+let chute = prompt('Escolha um número entre 1 e 10');
+
+    if (chute == numeroSecreto) {
+        alert(`você descobriu o número secreto ${numeroSecreto}! Parabéns!`);
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor do que ${chute}`);
+        } else {
+            alert (`O número secreto é maior do que ${chute}`);
+        }
+    }
+
